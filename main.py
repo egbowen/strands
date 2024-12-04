f409b198e674521fb411dafb36b7ddf67f8379c3
Liz <egbowen@middlebury.edu>
2024-12-04 15:07:00 -0500

    dfs is way more optimized thank goodness

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #make object with 6x8 board
 import boards
 from typing import Dict, List, Optional, Set, Tuple
+import time
 
 board = boards.day_11_19 #should not be global at some point 
 SOLUTION = boards.solution_11_19 #compare against this is global?
 SOLUTION_TOT = len(SOLUTION)
 WIDTH = 6
 HEIGHT = 8
-MAX_LEN = 11
+MAX_LEN = 11 #upping this number makes it take a long time
 
 SOLUTION_COUNT = 0
 
@@ -24,20 +25,21 @@ class dfsSimple:
         '''
         # display board
         neighbors = self.getNeighbors() #letters on each side (should we prune this as we fill in?) 
-        self.getDomains(neighbors) #all possible word combinations
-        return 
-
-    def getDomains(self, neighbors):
-        '''
-        Get all possible combinations of letters from 4 letters to MAX_LEN
         
-        Args:
-            board List(char): List of letters at ea position
-            neighbors: List(List(int)): indices of neighbors
-        '''
+        #run the tests
         for i in range(WIDTH*HEIGHT):
-            self.explore(i, [i], neighbors)
-        return 
+            # self.explore(i, [i], neighbors)
+            if (self.solution_count < SOLUTION_TOT): #stops when we find all solutions
+                self.explore(i, [i], neighbors)
+            else:
+                break
+            
+        print("You are done!")
+        if self.solution_count >= SOLUTION_TOT:
+            print("You solved the puzzle! Good job!")
+            return True
+        print("You did not solve the puzzle. Better luck next time :(")
+        return False 
 
     def explore(self, index: int, path: List[int], neighbors: List[List[int]]) -> List[List[int]]:
         '''
@@ -52,40 +54,40 @@ class dfsSimple:
         Returns:
             List[List[int]]: All possible paths explored from the current index
         '''
-        if len(path) >= MAX_LEN:
-            return []
+        found_something = False
         
-        #right now, this is checking every time we come across a path
-        if path in self.solution :
-            self.solution_count += 1
-            print(f"I found solution {self.solution_count}! Path: {path}")
+        while not found_something:
+            if len(path) >= MAX_LEN:
+                return []
             
-            # when we have found all solutions
-            if self.solution_count == self.solution_tot:
-                print("Congrats! You found all the solutions!")
-                return 
-            
-            #get rid of the values in all neighbors
-            # aka apply new constraint
-            new_neighbors = []
-            for n in neighbors:
-                # Create a new list excluding the values that are in the path
-                new_n = [val for val in n if val not in path]
-                new_neighbors.append(new_n)
-            neighbors = new_neighbors
-        
-        all_paths = []
-        
-        if 4 <= len(path) <= MAX_LEN:
-            all_paths.append(path)
+            #right now, this is checking every time we come across a path
+            if 4 <= len(path) <= MAX_LEN:
+                if path in self.solution :
+                    self.solution_count += 1
+                    print(f"I found solution {self.solution_count}! Path: {path}")
+                    
+                    # when we have found all solutions
+                    if self.solution_count == self.solution_tot:
+                        print("Congrats! You found all the solutions!")
+                        found_something = True
+                        break
+                        # here we need to get out of everything
+                    
+                    #get rid of the values in all neighbors
+                    new_neighbors = []
+                    for n in neighbors:
+                        # Create a new list excluding the values that are in the path
+                        new_n = [val for val in n if val not in path]
+                        new_neighbors.append(new_n)
+                    neighbors = new_neighbors
 
-        for neighbor in neighbors[index]:
-            if neighbor not in path:
-                #print(f"Exploring neighbor {neighbor} from index {index} with path {path}") 
-                new_path = path + [neighbor]  
-                all_paths.extend(self.explore(neighbor, new_path, neighbors))  # Add all resulting paths to the list
-
-        return all_paths
+            for neighbor in neighbors[index]:
+                if neighbor not in path:
+                    #print(f"Exploring neighbor {neighbor} from index {index} with path {path}") 
+                    new_path = path + [neighbor]  
+                    self.explore(neighbor, new_path, neighbors)
+            found_something = True
+        return []
         
     def getNeighbors(self):
         moves = [-1, 1, -WIDTH, WIDTH, -WIDTH - 1, -WIDTH + 1, WIDTH - 1, WIDTH + 1]
@@ -117,11 +119,19 @@ class WordRules(dfsSimple):
     def wordRulesSolver(self):
         neighbors = dfsSimple.getNeighbors(self)
            
-        # run game
+        #run the tests
         for i in range(WIDTH*HEIGHT):
-            self.explore(i, [i], neighbors)
-        print("Game Over!")
-        return
+            if (self.solution_count < SOLUTION_TOT): #stops when we find all solutions
+                self.explore(i, [i], neighbors)
+            else:
+                break
+            
+        print("You are done!")
+        if self.solution_count >= SOLUTION_TOT:
+            print("You solved the puzzle! Good job!")
+            return True
+        print("You did not solve the puzzle. Better luck next time :(")
+        return False 
         
     def explore(self, index: int, path: List[int], neighbors: List[List[int]]) -> List[List[int]]:
         '''
@@ -146,7 +156,7 @@ class WordRules(dfsSimple):
             # when we have found all solutions
             if self.solution_count == self.solution_tot:
                 print("Congrats! You found all the solutions!")
-                return 
+                return []
             
             #get rid of the values in all neighbors
             # aka apply new constraint
@@ -159,6 +169,7 @@ class WordRules(dfsSimple):
         
         all_paths = []
         
+        # need to make it so if we have returned [] we stop looking at that one
         
         if 4 <= len(path) <= MAX_LEN:
             if len(path) == 4:
@@ -303,99 +314,30 @@ def displayBoard(board: List[str]):
         print("| " + " | ".join(row) + " |") # rows with letters 
         print("_" * (WIDTH * 4 + 1)) #horizontal border
 
-# run   
+
+
+# run word rules  
+# print("WORD_RULES SOLVER")
 # displayBoard(board)
 # wordy_solver = WordRules(board, SOLUTION)  
-# wordy_solver.wordRulesSolver() 
+# wordy_starttime = time.time() 
+# word_solved = wordy_solver.wordRulesSolver() 
+# wordy_endtime = time.time() 
+
+# wordy_time = wordy_endtime - wordy_starttime
+
 
 #run greedy
+print("GREEDY SOLVER")
 displayBoard(board)
-dfs_solver = dfsSimple(board, SOLUTION)  
-dfs_solver.greedySolver() 
+dfs_solver = dfsSimple(board, SOLUTION) 
+greedy_starttime = time.time()  
+dfs_solved = dfs_solver.greedySolver() 
+greedy_endtime = time.time() 
 
+greedy_time = greedy_endtime - greedy_starttime
 
+#final outcomes:
+#print(f"Word Rules Solver: Time taken = {wordy_time:.2f} seconds, Solved = {word_solved}")
+print(f"Greedy Solver: Time taken = {greedy_time:.2f} seconds, Solved = {dfs_solved}")
 
-
-
-
-
-
-
-
-
-
-######## ABE FRAMEWORK #######
-
-# def generateDomains(board: List[str], neighbors: List[List[int]]): 
-#     '''
-#     generate domain of possible words for each letter tile
-#     '''
-#     domains = [] 
-#     for i in range(WIDTH * HEIGHT): 
-#         all_paths = explore(i, [i], neighbors) #explore paths starting at index 'i' 
-#         words = ["".join(board[j] for j in path) for path in all_paths] 
-#         domains.append(words) 
-#     return domains 
-
-
-# def customSolver(board: List[str], neighbors: List[List[int]], solution: List[List[int]]): 
-#     '''
-#     custom solver modeled as CSP
-#     '''
-#     domains = generateDomains(board, neighbors)
-#     assignment = {} 
-
-#     def backtrack(): 
-#         if len(assignment) == WIDTH * HEIGHT: 
-#             return assignment 
-        
-#         #select an unassigned tile 
-#         var = selectUnassignedTile(domains, assignment) 
-#         if var is None: 
-#             return None 
-        
-#         #try assigning word from its domain 
-#         for word in domains[var]: 
-#             if isConsistent(word, assignment): 
-#                 #assign and propagate constraints 
-#                 assignment[var] = word 
-#                 propagateConstraints(domains, word) 
-
-#                 result = backtrack() 
-#                 if result is not None: 
-#                     return result 
-                
-#                 del assignment[var] 
-#         return None 
-
-#     return backtrack() 
-
-# def selectUnassignedTile(domains: List[List[str]], assignment: Dict[int, str]) -> Optional[int]: 
-#     '''
-#     select next UNASSIGNED tile... use heuristic? 
-#     '''
-#     for i in range(WIDTH * HEIGHT): 
-#         if i not in assignment: 
-#             return i 
-#     return None 
-
-# def isConsistent(word: str, assignment: Dict[int, str]) -> bool: 
-#     '''
-#     check consistency, apply constraints 
-#     '''
-#     # check letters are adjacent, not reused from already verified words 
-#     return True 
-
-# def propagateConstraints(domains: List[List[str]], word: str): 
-#     ''' 
-#     propagate constraints after assigning a word
-#     ex: remove letters of verified word from other domains, 
-#     '''
-#     pass
-
-    
-
-        
-        
-    
-    
