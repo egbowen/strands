e9dee9fc3ad2bf7215ee8a261dcea802619deb25
Liz <egbowen@middlebury.edu>
2024-12-07 16:17:03 -0500

    added a board

--- boards.py
+++ boards.py
@@ -21,8 +21,29 @@ solution_11_19 = [
     ]
 
 clue_11_19 = "Pick-Ups"
+height_11_19 = 8
+width_11_19 = 6
 
-test_board = ["a", "b", "c", "d"]
+#################################################################
+#sample smaller board 
+small_board = ['M', 'A', 'C', 'E', 
+            'O', 'R', 'N', 'S', 
+            'N', 'R', 'E', 'E', 
+            'E', 'G', 'M', 'M', 
+            'I', 'O', 'I', 'I', 
+            'R', 'S', 'C', 'F']
+small_solution = [
+    [5, 4, 0, 1, 2], #romance
+    [13, 12, 8, 9, 10, 7], #genres
+    [15, 11, 14, 17, 16, 20], #memoir
+    [21, 22, 18, 23, 19] #scifi
+    ]
+
+small_clue = "reading material"
+
+small_height = 6
+small_width = 4
+
 
 
 # code to check the boards
